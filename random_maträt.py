11a93279afab5f2407e70b46fb17f95c7afc5ccd
shadowninja71 <viktor.hvidpersson@elev.ga.ntig.se>
2025-04-30 09:32:00 +0200

    fixade problem

--- random_maträt.py
+++ random_maträt.py
@@ -19,16 +19,10 @@ def show_desär():
     efterrätt = random.choice(efterrätter)
     text_Box.config(text=efterrätt)
 
-def show_skriv():
-    skriv = e1 = Entry(root) 
-    e1.config(row=0, column=1)
-
 show_button1 = Button(root, text="Få en random måltid", width=50, command=show_main)
 show_button1.pack()
 show_button2 = Button(root, text="Få en random efterrätt", width=50, command=show_desär)
 show_button2.pack()
-show_button3 = Button(root, text="First Name", width=50, command=show_skriv)
-show_button3.pack()
 Button = Button(root, text="Avsluta", width=50, command=root.destroy)
 Button.pack()
 
